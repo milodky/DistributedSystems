aaf72acf20f5240b306c8855b7664cae3504963d
Suvodeep Pyne <pyne.suvodeep@gmail.com>
2013-02-06 00:06:04 -0600

    Basic communication is working fine.

--- client.cpp
+++ client.cpp
@@ -5,7 +5,18 @@ void connect()
 {
 	LSP_Client client;
 	client.connect();
-	client.run();
+	char send_data[1024];
+
+	while (1)
+	{
+		printf("Type Something (q or Q to quit):");
+		gets(send_data);
+
+		if ((strcmp(send_data , "q") == 0) || strcmp(send_data , "Q") == 0)
+			break;
+		else
+			client.send_message(LOCALHOST, 5000, send_data);
+	}
 }
 
 int main ()

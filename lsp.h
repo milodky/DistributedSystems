317e1e77beeaff28f37feaa9f7e7099a08fec077
Suvodeep Pyne <pyne.suvodeep@gmail.com>
2013-02-06 00:07:40 -0600

    Basic communication is working fine.

--- lsp.h
+++ lsp.h
@@ -60,26 +60,39 @@ public:
 		return setupTalker(LOCALHOST, SERVER_PORT);
 	}
 
-	void run()
+	/**
+	 * Default send: send to server
+	 */
+	void send_message(char* const msg)
 	{
-		char send_data[1024];
-
-		while (1)
+		if (sendto(sockfd, msg, strlen(msg), 0,
+				ai_node->ai_addr, ai_node->ai_addrlen) == -1)
 		{
-			printf("Type Something (q or Q to quit):");
-			gets(send_data);
-
-			if ((strcmp(send_data , "q") == 0) || strcmp(send_data , "Q") == 0)
-				break;
-			else
-				if (sendto(sockfd, send_data, strlen(send_data), 0,
-						ai_node->ai_addr, ai_node->ai_addrlen) == -1)
-				{
-					perror("talker: sendto");
-
-				}
+			perror("talker: sendto");
 		}
+	}
 
+	/**
+	 * Explicitly mention the recipient hostname and recipient port.
+	 * Same socket can be used to send to different recipients.
+	 */
+	void send_message(char* const recvr_hostname, const int recvr_port, char* const msg)
+	{
+		struct sockaddr_in recvr_addr;
+		struct hostent *host;
+
+		host = (struct hostent *) gethostbyname((char *) recvr_hostname);
+
+		recvr_addr.sin_family = AF_INET;
+		recvr_addr.sin_port = htons(recvr_port);
+		recvr_addr.sin_addr = *((struct in_addr *) host->h_addr);
+		bzero(&(recvr_addr.sin_zero), 8);
+
+		if (sendto(sockfd, msg, strlen(msg), 0,
+				(struct sockaddr *)&recvr_addr, sizeof(struct sockaddr)) == -1)
+		{
+			perror("talker: sendto");
+		}
 	}
 
 	virtual ~LSP_Client()

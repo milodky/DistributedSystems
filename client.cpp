3fa0730dcd3d7a1376684c6d42bdacf79e217c5e
svenketr <sanu13@gmail.com>
2013-02-17 13:15:39 -0600

    checking format of arguments

--- client.cpp
+++ client.cpp
@@ -6,27 +6,17 @@ void connect(char* host, char *port, char* hash, char* len)
 	LSP_Requester request(host,port,hash,atoi(len));
 	request.init();
 	request.run();
-	/*
-	LSP_Client client;
-	client.connect();
-	char send_data[1024];
-
-	while (1)
-	{
-		printf("Type Something (q or Q to quit):");
-		gets(send_data);
-
-		if ((strcmp(send_data , "q") == 0) || strcmp(send_data , "Q") == 0)
-			break;
-		else
-			client.send_message(LOCALHOST, 5000, send_data);
-	}
-	*/
 }
 
 int main (int argc, char* const argv[])
 {
 	if(argc!=4)
+	{
+		printf("Incorrect usage. Please use format as below.\n");
+		printf("./request host:port hash len\n");
+		return 1;
+	}
+	if(strchr(argv[1],':')==NULL)
 	{
 		printf("Incorrect usage. Please use format as below.\n");
 		printf("./request host:port hash len\n");
--- worker.cpp
+++ worker.cpp
@@ -6,33 +6,6 @@
 
 using namespace std;
 
-string numToString(int x,int length)
-{
-	string str = "";
-	int y;
-	do {
-		y=x%26;
-		str=char('a'+y) +str;
-		x/=26;
-	}while(x);
-	str.insert(0,length-str.length(),'a');
-	return str;
-}
-
-void searchIn(string password, int start, int end, int length)
-{
-	for(int i = start; i < end; i++)
-	{
-		string str = numToString(i,length);
-		cout << str << endl;
-		if(str.compare(password)==0)
-		{
-			cout<<"Password Matched!  " << str <<endl;
-			break;
-		}
-	}
-}
-
 void connect(char* host, char *port)
 {
 	LSP_Worker worker(host,port);
@@ -48,27 +21,17 @@ int main(int argc, char** argv)
 		printf("./worker host:port\n");
 		return 1;
 	}
+	if(strchr(argv[1],':')==NULL)
+	{
+		printf("Incorrect usage. Please use format as below.\n");
+		printf("./worker host:port\n");
+		return 1;
+	}
 
 	char* t1 = strtok(argv[1],":");
 	char* host = t1;
 	t1 = strtok(NULL,":");
 	char* port = t1;
-	cout<<port<<endl;
 	connect(host,port);
-	int i = 0;
-	unsigned char temp[SHA_DIGEST_LENGTH];
-	char buf[SHA_DIGEST_LENGTH*2];
-
-	memset(buf, 0x0, SHA_DIGEST_LENGTH*2);
-	memset(temp, 0x0, SHA_DIGEST_LENGTH);
-	char* password = "aadef";
-
-	SHA1((unsigned char *)password, strlen(password), temp);
-
-	for (i=0; i < SHA_DIGEST_LENGTH; i++) {
-		sprintf((char*)&(buf[i*2]), "%02x", temp[i]);
-	}
-
-	printf("SHA1 of %s is %s\n", password, buf);
 	return 0;
 }
